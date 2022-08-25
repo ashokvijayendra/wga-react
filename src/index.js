3775634007b1871f5c468d48f42ca604d63ffe47
Ashok Vijayendra <ashok.vijayendra@publicissapient.com>
2022-08-25 01:00:59 -0500

    removing web vitals so that only one chunk is created

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const RefreshComponents = (refresh = false) => {
   document.querySelectorAll('[data-reactcomponent]').forEach((ele) => {
     const root = ele.root ||  ReactDOM.createRoot(ele);
     ele.root = root;
     root.render(
-        <App component={ele.dataset.reactcomponent} url={ele.dataset.url}/>
+        <App component={ele.dataset.reactcomponent} variant={ele.par} url={ele.dataset.url}/>
     );
   });
 }
 
 document.addEventListener("DOMContentLoaded", RefreshComponents);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
