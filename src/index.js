b8dd71c15cd9c92c2b02904f55e9e656d92fb21c
Ashok Vijayendra <ashok.vijayendra@publicissapient.com>
2022-08-24 23:51:31 -0500

    adding capability to render components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const RefreshComponents = (refresh = false) => {
+  document.querySelectorAll('[data-reactcomponent]').forEach((ele) => {
+    const root = ele.root ||  ReactDOM.createRoot(ele);
+    ele.root = root;
+    root.render(
+        <App component={ele.dataset.reactcomponent} url={ele.dataset.url}/>
+    );
+  });
+}
+
+document.addEventListener("DOMContentLoaded", RefreshComponents);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
